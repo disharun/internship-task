5eec60882c417c069c1c6d8c1ff0b9e3ead12680
disharun <vishdisha005@gmail.com>
2025-08-18 21:29:26 +0530

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,35 +1,16 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-// Support both React 18+ (react-dom/client) and older react-dom versions (v17)
-// Try to use the new createRoot API; fall back to ReactDOM.render when
-// 'react-dom/client' is not available (e.g., older installs on some CI/envs).
-try {
-  // Dynamically require to avoid build-time resolution errors when the
-  // module doesn't exist on the environment.
-  // eslint-disable-next-line global-require
-  const { createRoot } = require("react-dom/client");
-  const root = createRoot(document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  );
-} catch (e) {
-  // Fallback for environments with react-dom v17 where 'react-dom/client'
-  // does not exist.
-  // eslint-disable-next-line global-require
-  const ReactDOM = require("react-dom");
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
